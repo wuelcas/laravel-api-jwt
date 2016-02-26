9356b7ee0261e7029fb3132a5fdca57e0a128c4c
Wuelber Castillo <wuelber.castillo@gmail.com>
2016-02-26 14:55:53 -0600

    Adding js files and templates and setting the view for angular frontend

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+var bookWishlistApp = angular.module('bookWishlistApp', [
+    'ngRoute',
+    'bookWishlistAppControllers',
+    'bookWishlistAppServices'
+]);
+
+bookWishlistApp.config(['$routeProvider', function($routeProvider) {
+    
+    $routeProvider.
+    when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+    }).
+    when('/signup', {
+        templateUrl: 'partials/signup.html',
+        controller: 'SignupController'
+    }).
+    when('/', {
+        templateUrl: 'partials/index.html',
+        controller: 'MainController'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+
+}]);
